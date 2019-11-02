b2930e906329699fa9d81727e0c49b6e1c596cea
Vincent-Job <vinjob30@gmail.com>
2019-11-02 19:37:17 +0000

    image-blur1 updates

--- image-blur1.rb
+++ image-blur1.rb
@@ -10,6 +10,30 @@ class Image
       puts arr.join
     end
   end
+
+def blur
+  copy = Image.new(@image_data)
+  (0..@image_data.length).each do |row_index|
+    (0..@image_data[row_index].length).each do |column_index|
+      if @image_data[row_index][column_index] == 1
+        if row_index != 0 
+         # to neigbor
+         copy.image_data[row_index-1][column_index] = 1
+        
+         #bottom Neigbor
+         copy.image_data[row_index+1][column_index] = 1
+        
+         #left Neigbhor
+         copy.image_data[row_index][column_index-1] = 1
+        
+         copy.image_data[row_index][column_index+1] = 1
+        end  
+      end
+    end
+  end
+  @image_data = copy.image_data  
+
+end
 end
 
 image = Image.new([
@@ -18,5 +42,6 @@ image = Image.new([
   [0, 0, 0, 1],
   [0, 0, 0, 0]
 ]) 
+image.blur
 image.output_image
 
