fac9052ab0601f0f0561c83cf1f9bf8e054c0095
Vincent-Job <vinjob30@gmail.com>
2019-11-11 01:04:12 +0000

    image-blur1

--- image-blur1.rb
+++ image-blur1.rb
@@ -5,6 +5,18 @@ class Image
     @image_data = image_data
   end
 
+  def get_ones
+    ones = []
+    @image_data.each_with_index do |row, row_number|
+      row.each_with_index do |item, col_number|
+        if item == 1
+          ones << [row_number, col_number]
+        end
+      end
+    end
+    ones
+  end
+
   def output_image
     @image_data.each do |arr|
       puts arr.join
@@ -12,28 +24,69 @@ class Image
   end
 
 def blur
-  copy = Image.new(@image_data)
-  (0..@image_data.length).each do |row_index|
-    (0..@image_data[row_index].length).each do |column_index|
+  get_ones.each do |row_index, column_index|
+    if row_index != 0 
+     # top neigbor
+    @image_data[row_index-1][column_index] = 1
+   end
+    if row_index != @image_data.length - 1 
+     #bottom Neigbor
+     @image_data[row_index+1][column_index] = 1
+   end
+    if column_index != 0 
+     #left Neigbhor
+     @image_data[row_index][column_index-1] = 1
+   end
+    if column_index != @image_data[row_index].length - 1
+      # right nieghbor
+     @image_data[row_index][column_index+1] = 1
+    end  
+  end
+
+  
+  # def blur!(distance=1)
+  #  distance.times do
+   #   blur_coords!
+  #  end
+ # end
+
+end
+=begin
+def blur
+  #copy = Image.new(@image_data)  this is a shallow copy
+  copy = Image.new Marshal.load( Marshal.dump(@image_data) )
+  (0...@image_data.length).each do |row_index|
+    (0...@image_data[row_index].length).each do |column_index|
       if @image_data[row_index][column_index] == 1
         if row_index != 0 
-         # to neigbor
+         # top neigbor
          copy.image_data[row_index-1][column_index] = 1
-        
+       end
+        if row_index != @image_data.length - 1 
          #bottom Neigbor
          copy.image_data[row_index+1][column_index] = 1
-        
+       end
+        if column_index != 0 
          #left Neigbhor
          copy.image_data[row_index][column_index-1] = 1
-        
+       end
+        if column_index != @image_data[row_index].length - 1
+          # right nieghbor
          copy.image_data[row_index][column_index+1] = 1
         end  
       end
     end
   end
   @image_data = copy.image_data  
+  
+  # def blur!(distance=1)
+  #  distance.times do
+   #   blur_coords!
+  #  end
+ # end
 
 end
+=end
 end
 
 image = Image.new([
@@ -44,4 +97,6 @@ image = Image.new([
 ]) 
 image.blur
 image.output_image
+
+
 
