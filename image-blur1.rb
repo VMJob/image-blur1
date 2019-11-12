72fd757903890a7f233acc20e4c7485d23acdfda
Vincent-Job <vinjob30@gmail.com>
2019-11-12 17:51:29 +0000

    image-blur

--- image-blur1.rb
+++ image-blur1.rb
@@ -89,6 +89,30 @@ end
 =end
 end
 
+# Code to be edited
+def blur(blur_distance)
+    ones = get_ones
+
+    @image_data.each_with_index do |row, row_number|
+      row.each_with_index do |item, col_number|
+        ones.each do |found_row_number, found_col_number|
+          if manhattan_distance(col_number, row_number, found_col_number, found_row_number) <= blur_distance
+            @image_data[row_number][col_number] = 1
+          end
+        end
+      end
+    end
+  end
+  
+=begin
+  def manhattan_distance (x1, y1, x2, y2)
+    horizontal_distance = (x2 - x1).abs
+    vertical_distance = (y2 - y1).abs
+    # (x2 - x1).abs + (y2 - y1).abs
+    horizontal_distance + vertical_distance
+  end
+=end 
+
 image = Image.new([
   [0, 0, 0, 0],
   [0, 1, 0, 0],
