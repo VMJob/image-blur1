d4120bde124ea061481868d1f254e4a4744eb51f
Vincent-Job <vinjob30@gmail.com>
2019-11-16 00:37:36 +0000

    blur3

--- image-blur1.rb
+++ image-blur1.rb
@@ -1,48 +1,65 @@
 class Image 
-  attr_accessor :image_data
-  
-  def initialize(image_data)
-    @image_data = image_data
-  end
+    attr_accessor :image_data
+    
+    def initialize(image_data)
+      @image_data = image_data
+    end
 
-  def get_ones
-    ones = []
-    @image_data.each_with_index do |row, row_number|
-      row.each_with_index do |item, col_number|
-        if item == 1
-          ones << [row_number, col_number]
+    def get_ones
+      ones = []
+      @image_data.each_with_index do |row, row_number|
+        row.each_with_index do |item, col_number|
+          if item == 1
+            ones << [row_number, col_number]
+          end
         end
       end
+      ones
     end
-    ones
+
+    def output_image
+      @image_data.each do |arr|
+        puts arr.join
+      end
+    end
+
+  def blur
+    get_ones.each do |row_index, column_index|
+      if row_index != 0 
+       # top neigbor
+      @image_data[row_index-1][column_index] = 1
+     end
+      if row_index != @image_data.length - 1 
+       #bottom Neigbor
+       @image_data[row_index+1][column_index] = 1
+     end
+      if column_index != 0 
+       #left Neigbhor
+       @image_data[row_index][column_index-1] = 1
+     end
+      if column_index != @image_data[row_index].length - 1
+        # right nieghbor
+       @image_data[row_index][column_index+1] = 1
+      end  
+    end
   end
 
-  def output_image
-    @image_data.each do |arr|
-      puts arr.join
+  def blur2(blur_distance)
+    blur_distance.times do 
+      blur 
     end
-  end
-
-def blur
-  get_ones.each do |row_index, column_index|
-    if row_index != 0 
-     # top neigbor
-    @image_data[row_index-1][column_index] = 1
-   end
-    if row_index != @image_data.length - 1 
-     #bottom Neigbor
-     @image_data[row_index+1][column_index] = 1
-   end
-    if column_index != 0 
-     #left Neigbhor
-     @image_data[row_index][column_index-1] = 1
-   end
-    if column_index != @image_data[row_index].length - 1
-      # right nieghbor
-     @image_data[row_index][column_index+1] = 1
-    end  
-  end
+  end 
 end
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+]) 
+image.blur2 (2)
+image.output_image
+
 =begin
 def blur
   #copy = Image.new(@image_data)  this is a shallow copy
@@ -71,39 +88,6 @@ def blur
   end
   @image_data = copy.image_data  
   
-  # def blur!(distance=1)
-  #  distance.times do
-   #   blur_coords!
-  #  end
- # end
-
-end
+  
 =end
-end
-
-# Code to be edited
-def blur(blur_distance)
-    ones = get_ones
-
-    @image_data.each_with_index do |row, row_number|
-      row.each_with_index do |item, col_number|
-        ones.each do |found_row_number, found_col_number|
-          if manhattan_distance(col_number, row_number, found_col_number, found_row_number) <= blur_distance
-            @image_data[row_number][col_number] = 1
-          end
-        end
-      end
-    end
-  end
-
-image = Image.new([
-  [0, 0, 0, 0],
-  [0, 1, 0, 0],
-  [0, 0, 0, 1],
-  [0, 0, 0, 0]
-]) 
-image.blur
-image.output_image
-
-
 
